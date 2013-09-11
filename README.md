93c982b4b5b2e093d4fb117190ed1c2b306f8bfb
zhangyunlong <zhangyunlong@baidu.com>
2013-09-11 23:45:01 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+## an optimizer for fis to remove alert, console, console.log, debugger etc. 
+
+## step 1. install
+
+```bash
+npm install -g fis-optimizer-shutup
+```
+
+## step 2. configurate
+
+```javascript
+
+//fis-conf.js
+fis.config.set('modules.optimizer.js', 'shutup, uglify-js');
+
+```
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function(content){
-    return content.replace(/"(?:[^\\"\r\n\f]|\\[\s\S])*"|'(?:[^\\'\n\r\f]|\\[\s\S])*'|(?:\/\/[^\r\n\f]+|\/\*[\s\S]*?(?:\*\/|$))|(^|[^.\w])(alert|console|console(\.\w+)+)\s*\([^()]+\)\s*;?/g, function(m, $1, $2){
+    return content.replace(/"(?:[^\\"\r\n\f]|\\[\s\S])*"|'(?:[^\\'\n\r\f]|\\[\s\S])*'|(?:\/\/[^\r\n\f]+|\/\*[\s\S]*?(?:\*\/|$))|(^|[^.\w])(debugger|(?:alert|console|console(\.\w+)+)\s*\([^()]+\))\s*;?/g, function(m, $1, $2){
         if($2) return $1;
         return m;
     }); 

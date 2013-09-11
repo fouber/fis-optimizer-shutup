98b33bd679983a2b767f49be9423af7c6268ab13
zhangyunlong <zhangyunlong@baidu.com>
2013-09-11 23:40:14 +0800

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = function(content){
+    return content.replace(/"(?:[^\\"\r\n\f]|\\[\s\S])*"|'(?:[^\\'\n\r\f]|\\[\s\S])*'|(?:\/\/[^\r\n\f]+|\/\*[\s\S]*?(?:\*\/|$))|(^|[^.\w])(alert|console|console(\.\w+)+)\s*\([^()]+\)\s*;?/g, function(m, $1, $2){
+        if($2) return $1;
+        return m;
+    }); 
+};
